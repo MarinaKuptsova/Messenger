3b374435c466ef5508588f53fcd4c0e3178ceb8c
MarinaKuptsova <marina55596@yandex.ru>
2017-10-22 23:51:22 +0300

    models, datalayer, api

---
+++ Messenger/Messenger.Api/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Messenger.DataLayer;
+using Messenger.DataLayer.Sql;
+using Messenger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Messenger.Api.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        private readonly IUsersRepository _usersRepository;
+        private const string ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Messenger;MultipleActiveResultSets=True;User Id=Marina; Password=1234Ma";
+        
+
+        public UsersController()
+        {
+            _usersRepository = new UsersRepository(ConnectionString);
+        }
+
+        [HttpGet]
+        [Route("api/users/{id}")]
+        public User Get(Guid id)
+        {
+            return _usersRepository.Get(id);
+        }
+
+        [HttpDelete]
+        [Route("api/users/{id}")]
+        public void Delete(Guid id)
+        {
+            _usersRepository.Delete(id);
+        }
+    }
+}
